521da1204f7b134e3626c8715171c518fd48f3a3
vsaitejaswie <saitejaswie@gmail.com>
2018-04-08 18:26:08 +0530

    Read from URL

--- first.py
+++ first.py
@@ -5,18 +5,40 @@ app = Flask(__name__)
 app.debug = True
 
 @app.route('/', methods = ['GET', 'POST'])
-def me():
+def login_form_main():
 
 	error = None
 	if request.method == "POST":
 		if request.form['username'] != 'admin' or request.form['password'] != 'admin' :
 			error = "Invalid Credentials. Please Try Again"
 		else:
-			return redirect(url_for('hom'))
+			return redirect(url_for('login_status'))
 
 
 	return render_template("index.html", error = error)
 
-@app.route('/home')
-def hom():
+@app.route('/status')
+def login_status():
 	return render_template('home.html')
+
+@app.route('/test', methods = ['GET', 'POST'])
+def login_form_test():
+
+	error = None
+
+	if request.method == 'POST': 
+		name_info = request.form['username']
+		return redirect(url_for('print_info', uname = name_info))
+		#return redirect(url_for('print_info', uname_alter = name_info))
+
+	return render_template('index.html', error = error)
+
+@app.route('/test/info/', methods = ['GET', 'POST'])
+def print_info():
+	u_name = request.args.get('uname')
+	return render_template('print.html', u__name = u_name)
+
+@app.route('test/info/alternative/<uname_alter>', methods = ['GET', 'POST'])
+def print_info_alter(uname_alter):
+	u_name_alter = uname_alter
+	return render_template('print.html', u__name = u_name_alter)
